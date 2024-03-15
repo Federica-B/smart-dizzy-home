c9374fef9c27cff6ea20e00f202858a5518b1f7f
Federica-B <federica.barbi99@gmail.com>
2024-03-15 18:51:01 +0100

    added polling in request temperature

--- raspy/raspy_serial_full_duplex_arduino/serial_read_write_mqtt_clients.py
+++ raspy/raspy_serial_full_duplex_arduino/serial_read_write_mqtt_clients.py
@@ -19,7 +19,7 @@ simp =threading.Semaphore(1)
 
 def initilization_serial():
     try:
-        arduino_serial_acmzero = serial.Serial('/dev/ttyACM0', 9600)  # Su Linux/Mac
+        arduino_serial_acmzero = serial.Serial('/dev/ttyACM0', 9600, timeout= 1)  # Su Linux/Mac
     except:
         print("Error in connecting with the serial - but the show must go on")
         arduino_list_serial.append(0)
@@ -28,7 +28,7 @@ def initilization_serial():
         arduino_list_serial.append(arduino_serial_acmzero)
     
     try:
-        arduino_serial_acmone = serial.Serial('/dev/ttyACM1', 9600)  # Su Linux/Mac
+        arduino_serial_acmone = serial.Serial('/dev/ttyACM1', 9600, timeout= 1)  # Su Linux/Mac
     except:
         print("Error in connecting with the serial - but the show must go on")
         arduino_list_serial.append(0)
@@ -37,7 +37,7 @@ def initilization_serial():
         arduino_list_serial.append(arduino_serial_acmone)
 
     try:
-        arduino_serial_acmtwo = serial.Serial('/dev/ttyACM2', 9600)  # Su Linux/Mac
+        arduino_serial_acmtwo = serial.Serial('/dev/ttyACM2', 9600, timeout= 1)  # Su Linux/Mac
     except:
         print("Error in connecting with the serial - but the show must go on")
         arduino_list_serial.append(0)
@@ -103,21 +103,23 @@ def temperature_mqtt_task():
             print(f"Failed to connect, return code {rc}")
 
     print("Task temperature mqtt assigned to thread: {}".format(threading.current_thread().name))
-    print("ID of process running task 1: {}".format(os.getpid()))
+    print("ID of process running task 2: {}".format(os.getpid()))
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
     client.on_connect = on_connect
     client.connect(broker_address, port)
 
     ## data initilization for telemetry
     acquired_data = ""
+    polling_command = '2'
     while True:
         for ser_arduino in arduino_list_serial:
             if ser_arduino != 0 and ser_arduino.port in telemetry_acm_arduino:
                 print("Start serial communication with arduino! - Data read")
                 simp.acquire()
                 try:
-                    acquired_data = ser_arduino.read_until().decode('utf-8')  # Invia il dato come stringa codificata in bytes
-                    time.sleep(0.1)
+                    #print(polling_command.encode())
+                    ser_arduino.write(polling_command.encode())
+                    acquired_data = ser_arduino.readline().decode('utf-8')
                 except serial.SerialException as e:
                     print("Something went wrong when communicating with arduino serial! - NO DATA IS READ ON THE SERIAL PORT!!!!!!")
                     print(e)
@@ -130,7 +132,7 @@ def temperature_mqtt_task():
                     print("End correclty serial communication with arduino" + str(ser_arduino.name))
                 simp.release()
                 
-                if acquired_data != 0:
+                if len(acquired_data)>1:
                     acquired_data = acquired_data.split('\n')[0]
                     print("The data from port: "+ str(telemetry_acm_arduino) + " is: " + str(acquired_data))
                     client.publish(topic_telemetry, acquired_data)
