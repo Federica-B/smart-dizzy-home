7b50069cf14036205ecd5f20f37358e94a00a546
Federica-B <federica.barbi99@gmail.com>
2024-04-11 11:24:41 +0200

    kinda work - problem with CRC

--- raspy/raspy_scripts/raspy_serial_full_duplex_arduino/serial_read_write_mqtt_clients.py
+++ raspy/raspy_scripts/raspy_serial_full_duplex_arduino/serial_read_write_mqtt_clients.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt
 import time
 import threading
 import os
-
+from typing import Tuple
 
 from crc_package import crc
 # https://www.youtube.com/watch?v=s2bH-s4LI64 -> per crc submodule directory
@@ -18,14 +18,16 @@ from crc_package import crc
 broker_address = "127.0.0.1"
 port = 1883
 topic = "data/stress"
-topic_telemetry = "telemetry/temperature"
+topic_telemetry = "telemetry/"
 DEVICE_ID = "raspy"
 
-telemetry_acm_arduino = ['/dev/ttyACM0']
+telemetry_acm_arduino = ['/dev/ttyACM0', '/dev/ttyACM1']
 # TODO: condence them in a single dict
 arduino_list_serial = []
 arduino_id = {}
-simp =threading.Semaphore(1)
+STRESS_STATE = 0
+    ## change from semaphore to lock because need binary
+simp = threading.Lock()
 
 def initilization_serial():
     try:
@@ -55,35 +57,126 @@ def initilization_serial():
         print("Correcly initilize the serial of "+ str(arduino_serial_acmtwo.name))
         arduino_list_serial.append(arduino_serial_acmtwo)
 
-def requestId():
-    code_request_id = 773
-    request = str(code_request_id)+ ","+DEVICE_ID
+def controlCRC(data_list) -> bool:
+    msg = str(data_list[0])+ ","+ str(data_list[1])
+    b_msg= bytes(msg, 'utf-8')
+    crc_reponce = crc.crc_poly(b_msg, 16, 0x8005, crc=0xFFFF, ref_in=True, ref_out=True, xor_out=0xFFFF)
+    if  int(data_list[2]) == crc_reponce:
+        print("Correct CRC!!")
+        return True
+    else:
+        print("Incorrect CRC -> calculated:" + str(crc_reponce) + " recived" + str(data_list[2]))
+        return False
+
+def controlData(data) -> bool:
+    data_list = []
+    try:
+        data_list = data.replace("{", "").replace("}","").split(",")
+        if len(data_list) != 3:
+            print("Message recived with NO correct syntax!")
+            return False
+    except IndexError as e:
+        print(e)
+        return False 
+    except TypeError as te:
+        print("Message recived with NO correct syntax!")
+        print(te)
+        return False
+    else:
+        print("Message recived with correct syntax!")
+
+    control = controlCRC(data_list)
+    if not control: return False
+
+    if '400' == data_list[0]:
+        print("Error with the CRC")
+        return False
+    if '401' == data_list[0]:
+        print("Value string send to long - out of bound string - max axxepted 20 char")
+        return False
+    
+    return True
+
+
+def serialRequest(code_request, message, ser_arduino) -> Tuple[str,bool]:
+    acquired_data = ""
+    request = str(code_request)+ ","+str(message)
     msg = bytes(request, 'utf-8')
     crc_request = crc.crc_poly(msg, 16, 0x8005, crc=0xFFFF, ref_in=True, ref_out=True, xor_out=0xFFFF)
-
     request = bytes(str('{') + request+','+ str(crc_request)+str('}'), 'utf-8')
+    print("Request send: "+ str(request))
+    try:
+        #until i recive MY data im going no where!!!  - Arduino UNO is slow
+        counter = 0
+        while len(acquired_data) < 2 or counter < 3:
+            ser_arduino.write(request)
+            acquired_data = ser_arduino.read_until('\x7d').decode('utf-8')
+            counter = counter + 1
+    except serial.SerialException as e:
+        print("Something went wrong when communicating with arduino serial! - NO DATA IS READ ON THE SERIAL PORT!!!!!!")
+        print(e)
+        return 0
+    except TypeError as e:
+        print("Other stuff went wrong in the arduino communication")
+        print(e)
+        return 0
+    else:
+        print("End correclty serial communication with arduino" + str(ser_arduino.name))
+
+    if len(acquired_data) >2:
+        control = controlData(acquired_data)
+    else:
+        control = False
+        print("No information has been reviced!")
+    
+    if control:
+        print("Responce recived: "+ str(acquired_data))
+
+    return acquired_data, control
+
+def requestId() -> bool:
     for ser_arduino in arduino_list_serial:
         acquired_data = ""
         if 0 != ser_arduino:
             print("Start serial communication with arduino! - Request ID")
-            try:
-                #until i recive MY data im going no where!!!  - Arduino UNO is slow
-                while len(acquired_data) < 2:
-                    ser_arduino.write(request)
-                    acquired_data = ser_arduino.read_until('\x7d').decode('utf-8')
-        
-            except serial.SerialException as e:
-                print("Something went wrong when communicating with arduino serial! - NO DATA IS READ ON THE SERIAL PORT!!!!!!")
-                print(e)
-                return 0
-            except TypeError as e:
-                print("Other stuff went wrong in the arduino communication")
-                print(e)
-                return 0
+            acquired_data, control = settingID(ser_arduino)
+            if control:
+                arduino_id[ser_arduino.name] = acquired_data.replace("{", "").replace("}","").split(",")[1]
             else:
-                print("End correclty serial communication with arduino" + str(ser_arduino.name))
-            arduino_id[ser_arduino.name] = acquired_data.replace("{", "").replace("}","").split(",")[1]
-    
+                settingID(ser_arduino)
+    return control
+
+def settingID(ser_arduino):
+    code_request_id = 773
+    control = True
+    global DEVICE_ID
+    acquired_data, control = serialRequest(code_request_id, DEVICE_ID,ser_arduino)
+    return acquired_data, control
+
+def requestSensing(ser_arduino) -> Tuple[str,bool]:
+    global DEVICE_ID
+    code_request_sens = 786
+    control = True
+    sensing_val = ""
+    print("Start serial communication with arduino! - Request sensing value")
+    acquired_data, control = serialRequest(code_request_sens, DEVICE_ID,ser_arduino)
+    if control:
+        sensing_val = acquired_data.replace("{", "").replace("}","").split(",")[1]
+    return sensing_val, control
+
+def sendStress(ser_arduino) -> Tuple[str,bool]:
+    global STRESS_STATE
+    code_set_stress = 883
+    code_correct_stress = '283'
+    control = True
+    acquired_data, control = serialRequest(code_set_stress, STRESS_STATE,ser_arduino)
+    if control and code_correct_stress == acquired_data.replace("{", "").replace("}","").split(",")[0] :
+        print("Correcly set stress value")
+    else:
+        print("ENTROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+        sendStress(ser_arduino)
+    return acquired_data, control
+
 
 def task_mqtt_stress():
     def on_connect(client, userdata, flags, rc):
@@ -97,30 +190,28 @@ def task_mqtt_stress():
         print("New msg arrive from topic: "+ str(topic)+ "!")
         messaggio_stress = msg.payload.decode()
         print("The value of the message is: " + str(messaggio_stress))
-        send_data(messaggio_stress)
+        if messaggio_stress == '1' or messaggio_stress == '0':
+            send_data(int(messaggio_stress))
+        else:
+            print("Msg of stress not correct - not 0 or 1")
 
     def send_data(data):
         """
         Invia i dati alla porta seriale per Arduino.
         """
-        print("Start serial communication with arduino! - Data send")
-        for ser_arduino in arduino_list_serial:
-            if ser_arduino != 0:
-                simp.acquire()
-                try:
-                    ser_arduino.write(data.encode())  # Invia il dato come stringa codificata in bytes
-                    time.sleep(0.1)
-                except serial.SerialException as e:
-                    print("Something went wrong when communicating with arduino serial! - NO DATA IS WRITE ON THE SERIAL PORT!!!!!!")
-                    print(e)
-                    return 0
-                except TypeError as e:
-                    print("Other stuff went wrong in the arduino communication")
-                    print(e)
-                    return 0
-                else:
-                    print("End correclty serial communication with arduino" + str(ser_arduino.name))
-                simp.release()
+        global STRESS_STATE
+        responce = ""
+        control = True
+        if(data != STRESS_STATE):
+            STRESS_STATE = data
+            simp.acquire()
+            for ser_arduino in arduino_list_serial:
+                if 0 != ser_arduino:
+                    print("Start serial communication with arduino! - Set stress value")
+                    _,_ = sendStress(ser_arduino)
+            simp.release()
+        else:
+            print("No change needed to the stress value")
 
 
     print("Task MQTT assigned to thread: {}".format(threading.current_thread().name))
@@ -133,7 +224,7 @@ def task_mqtt_stress():
     client.loop_forever()
 
 
-def temperature_mqtt_task():
+def sensing_mqtt_task():
 
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
@@ -148,35 +239,22 @@ def temperature_mqtt_task():
     client.connect(broker_address, port)
 
     ## data initilization for telemetry
-    acquired_data = ""
-    polling_command = '2'
+    acquire_data = ""
+    control = True
     while True:
+        print("Start serial communication with arduino! - Data read")
         for ser_arduino in arduino_list_serial:
-            if ser_arduino != 0 and ser_arduino.port in telemetry_acm_arduino:
-                print("Start serial communication with arduino! - Data read")
+            acquired_data = ""
+            if 0 != ser_arduino:
                 simp.acquire()
-                try:
-                    #print(polling_command.encode())
-                    ser_arduino.write(polling_command.encode())
-                    acquired_data = ser_arduino.readline().decode('utf-8')
-                except serial.SerialException as e:
-                    print("Something went wrong when communicating with arduino serial! - NO DATA IS READ ON THE SERIAL PORT!!!!!!")
-                    print(e)
-                    return 0
-                except TypeError as e:
-                    print("Other stuff went wrong in the arduino communication")
-                    print(e)
-                    return 0
-                else:
-                    print("End correclty serial communication with arduino" + str(ser_arduino.name))
+                acquired_data, control = requestSensing(ser_arduino)
                 simp.release()
-                
-                if len(acquired_data)>1:
-                    acquired_data = acquired_data.split('\n')[0]
-                    acquired_data = acquired_data[:-1]
-                    print("The data from port: "+ str(telemetry_acm_arduino) + " is: " + str(acquired_data))
-                    client.publish(topic_telemetry, acquired_data)
-                    print("Data published on topic: "+ str(topic_telemetry))
+        
+            if len(acquired_data)>1 and control:
+                path = topic_telemetry + arduino_id[ser_arduino.name]
+                print("The data from port: "+ str(ser_arduino.name) + " is: " + str(acquired_data))
+                client.publish(path, acquired_data)
+                print("Data published on topic: "+ str(path))
                     
         time.sleep(10)
 
@@ -188,13 +266,15 @@ def main():
 
     initilization_serial()
     print("Serial trovate: " +str(arduino_list_serial))
-    requestId()
-    print("Device ID found!"+ str(arduino_id))
+    if requestId():
+        print("Device ID found! - The ID found are the following:"+ str(arduino_id))
+    else:
+        print("Some error occured during serial communication! - The ID found are the following:"+ str(arduino_id))
 
-    # t1 = threading.Thread(target=task_mqtt_stress, name='mqtt_task_stress')
-    # t2 = threading.Thread(target=temperature_mqtt_task, name='temperature_mqtt_task')
-    # t1.start()
-    # t2.start()
+    t1 = threading.Thread(target=task_mqtt_stress, name='mqtt_task_stress')
+    t2 = threading.Thread(target=sensing_mqtt_task, name='sensing_mqtt_task')
+    t1.start()
+    t2.start()
 
 
 
